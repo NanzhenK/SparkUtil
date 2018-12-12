657d1b3c94d84ae54546b7ee9833971fc2b59866
NanzhenK <nanzhen_kuang@hotmail.com>
2018-12-12 09:57:42 +0800

    Create SparkUtil.java

---
+++ SparkUtil.java
@@ -0,0 +1,16 @@
+import org.apache.spark.sql.SparkSession;
+
+public class SparkUtil {
+
+    public static SparkSession getSparkSession(String appName) {
+        return SparkSession
+                .builder()
+                .appName(appName)
+                .config("spark.sql.warehouse.dir", "hdfs://ns1/apps/hive/warehouse/ai.db")
+                .config("spark.sql.shuffle.partitions", 512)
+                .config("spark.driver.maxResultSize", "10g")
+                .enableHiveSupport()
+                .getOrCreate();
+    }
+
+}
