aa08c25ba0453b718557b6509051aa89d9e2fbd4
NanzhenK <nanzhen_kuang@hotmail.com>
2018-12-12 10:19:16 +0800

    Update ClassExecute.java

--- ClassExecute.java
+++ ClassExecute.java
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.*;
 
-public class ClassExecute {
+public class ClassExecute implements Serializable{
     private static final String SQL="";
 
     private SparkSession session;
@@ -12,6 +12,15 @@ public class ClassExecute {
     public void execute() {
         try {
             Dataset<Row> dataset = session.sql(SQL);
+            WindowSpec kw = Window.partitionBy("keyword").orderBy("keyword"); //分区标记
+            dataset
+                .mapPartitions((row)->{} , Encoders.kryo(ClassName.class)) //kryo用于自定义Serializable的class
+                .withColumn("value",split(col("value"),"\\|")) //分割符号前\\转义
+                .select(
+                    col("value").getItem(0).as("keyword"),
+                    col("value").getItem(1).as("count").cast("Int")
+                )
+                .withColumn("isrepeat", when(row_number().over(kw).equalTo(1),1).otherwise(0))//定义重复数据中第一项为1，其余为0
             dataset.write().mode(SaveMode.Overwrite).format("hive")
                     .insertInto("schemaName.dbName");
         } catch (Exception e) {
