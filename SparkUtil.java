bb376ff68c704bb34dc2e4e3b796002a63c5323a
NanzhenK <nanzhen_kuang@hotmail.com>
2018-12-12 10:01:00 +0800

    Update SparkUtil.java

--- SparkUtil.java
+++ SparkUtil.java
@@ -6,7 +6,7 @@ public class SparkUtil {
         return SparkSession
                 .builder()
                 .appName(appName)
-                .config("spark.sql.warehouse.dir", "hdfs://ns1/apps/hive/warehouse/ai.db")
+                .config("spark.sql.warehouse.dir", "hdfs://ns1/apps/hive/warehouse/dbname.db")
                 .config("spark.sql.shuffle.partitions", 1024) // 3 * num-executors * executor-cores
                 .config("spark.driver.maxResultSize", "10g")
                 .enableHiveSupport()
