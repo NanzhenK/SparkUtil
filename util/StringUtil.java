8a02df6cc8db331301af703d0743a727c1631d00
NanzhenK <nanzhen_kuang@hotmail.com>
2018-12-12 10:26:47 +0800

    Update StringUtil.java

--- util/StringUtil.java
+++ util/StringUtil.java
@@ -15,6 +15,25 @@ public class StringUtils {
         return s;
     }
     
+    public static String full2Half(String s){
+        return KeywordProcessor.process(Normalizer.normalize(s,Normalizer.Form.NFKC));
+    }
+    
+    public static String normalSpace(String s){
+        return s.replaceAll("[\\p{Zs}]"," ");
+    }
+    
+    public static String cleanPunctuation(String s){
+        return s.replaceAll("[\\pP\\pS\\pM\\pC&&[^.-]]"," "); // 除了 .- 以外的符号，用交集排除
+    }
+    
+    public stati String cleanNumbre(String s){
+        return s.replaceAll("[\\d]"," ");
+    }
+    
+        
+    
+    
     
 
 }
